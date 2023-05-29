ae43f287e12953d10c94d6c1b0b6f00c0a84bb20
Lesya-Brodskaya <110602282+Lesya-Brodskaya@users.noreply.github.com>
2023-05-29 19:06:07 +0300

    change  container

--- src/pages/Home/Home.styled.jsx
+++ src/pages/Home/Home.styled.jsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 
-export const Container = styled.container`
+export const Container = styled.div`
   min-height: calc(100vh - 50px);
   display: flex;
   align-items: center;
